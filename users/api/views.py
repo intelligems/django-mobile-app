6fd4e25a85cfc3866bd4e92d5eb9955e6c6b49c5
Kostas Livieratos <koslibpro@gmail.com>
2017-10-29 13:41:36 +0200

    extended user api endpoints

--- users/api/views.py
+++ users/api/views.py
@@ -1,20 +1,19 @@
-from rest_framework import status
 from rest_framework.decorators import permission_classes
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .serializers import AccountSerializer
 
 
-class MeApiHandler(APIView):
+class MeApiHandler(RetrieveUpdateAPIView):
     """
     API Endpoint that returns currently logged-in account's user information.
     This includes information that is not publicly-available.
     """
     permission_classes(IsAuthenticated)
+    parser_classes = [FormParser, MultiPartParser, JSONParser]
+    serializer_class = AccountSerializer
 
-    def get(self, request):
-        user = self.request.user
-        serializer = AccountSerializer(instance=user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get_object(self):
+        return self.request.user
